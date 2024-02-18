53c13a15c35ddc72bfa42d626113709ca3e11686
wildcardos-packer <wildcardos-packer>
2024-02-18 14:28:45 +0100

    remount before dmidecode

--- install/prepare_archiso/main.sh
+++ install/prepare_archiso/main.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
-log_text "Install dmidecode to detect execution inside a vm"
-archiso_pacman_whenneeded dmidecode
+log_text "Remount copy on write space"
+mount -o remount,size=75% /run/archiso/cowspace || true
 
+log_text "Make the journal log persistent on ramfs"
+mkdir -p /var/log/journal
+systemd-tmpfiles --create --prefix /var/log/journal
+systemctl restart systemd-journald
+
+log_text "Wait for pacman keyring init to be done"
+while ! systemctl show pacman-init.service | grep SubState=exited; do
+  systemctl --no-pager status -n0 pacman-init.service || true
+  sleep 5
+done
+
+log_text "Prepare nspawn environment to allow ufw firewall configuration"
+modprobe iptable_filter
+modprobe ip6table_filter
+
+log_text "Use dmidecode to detect execution inside a vm"
 DMIDECODE_SYSTEM_MANUFACTURER=$(dmidecode -s system-manufacturer)
 DMIDECODE_SYSTEM_PRODUCT_NAME=$(dmidecode -s system-product-name)
 RUNTIME_ENVIRONMENT_VMWARE=""
@@ -37,21 +53,3 @@ if [ -n "$RUNTIME_ENVIRONMENT_VIRTUALBOX" ]; then
 elif [ -n "$RUNTIME_ENVIRONMENT_QEMU" ]; then
   mount --mkdir -t 9p -o trans=virtio,version=9p2000.L,rw host.0 /share
 fi
-
-log_text "Remount copy on write space"
-mount -o remount,size=75% /run/archiso/cowspace || true
-
-log_text "Make the journal log persistent on ramfs"
-mkdir -p /var/log/journal
-systemd-tmpfiles --create --prefix /var/log/journal
-systemctl restart systemd-journald
-
-log_text "Wait for pacman keyring init to be done"
-while ! systemctl show pacman-init.service | grep SubState=exited; do
-  systemctl --no-pager status -n0 pacman-init.service || true
-  sleep 5
-done
-
-log_text "Prepare nspawn environment to allow ufw firewall configuration"
-modprobe iptable_filter
-modprobe ip6table_filter
