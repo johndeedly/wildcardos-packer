3ea685187512b6d197587225fcc3fdc451be626d
wildcardos-packer <wildcardos-packer>
2024-04-11 17:51:28 +0200

    Fix a rare bug when the filesystem package needs to be updated while resolv.conf is read-only bind mounted

--- install/nspawn-chroot.sh
+++ install/nspawn-chroot.sh
@@ -4,4 +4,5 @@
 scriptdir=$(realpath $(dirname $0))
 chroot="$1"
 shift
+# TODO, maybe? Bug with "--resolv-conf=bind-host" when guest tries to modify resolv.conf in pacman steps
 exec systemd-nspawn --capability=CAP_SYS_CHROOT,CAP_NET_ADMIN,CAP_NET_RAW --resolv-conf=bind-host --link-journal=host --bind="$scriptdir" --setenv="SCRIPTDIR=$scriptdir" --console=pipe -qD "$chroot" -- "$@"
